3dae995f645e5bd56476afa70a4ea10b8fe1a0ba
rafael <rafael.lixa@ua.pt>
2022-10-18 21:11:59 +0100

    Exerise 4 Start

---
+++ lab2/lab2_4/lab_2_4/src/test/java/com/ies/lab_2_4/Lab24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ies.lab_2_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
